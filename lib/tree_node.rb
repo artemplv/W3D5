bfe9b0bce374a6511b2f3b921b863cdd07629395
artemplv <artemplv@gmail.com>
2023-05-19 14:33:12 -0400

    add parents reassigning

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,17 +1,18 @@
 class PolyTreeNode
     attr_reader :parent, :children , :value
-    attr_writer :parent
+    # attr_writer :parent
+
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
-
     end
 
-    def parent=(node)   
-        parent = node
-        node.children << self
-
+    def parent=(node)
+        parent.children.delete(self) if parent
+    
+        @parent = node
+        @parent.children << self if @parent != nil
     end
 
 
